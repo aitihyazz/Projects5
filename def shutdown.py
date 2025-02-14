42b73a9ed711e18a50d8efa0dc632a2a8d96ad56
aitihyazz <Your GitHub Email>
2025-02-14 21:10:45 +0600

    desf sutdown

---
+++ def shutdown.py
@@ -0,0 +1,10 @@
+
+
+
+def all(a):
+    if a<100:
+        return("Sutting down")
+    else:
+        return("no need to sutdown")
+hi=int(input("What is your battary percentage:"))
+print(all(hi))        
